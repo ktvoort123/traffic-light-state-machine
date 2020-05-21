cf3913c0d082e0751d70f30bbc408dc8c255277d
Katie <katie.vandervoort@gmail.com>
2020-05-20 20:17:38 -0700

    implemented service.onTransition for wave 2

--- src/App.js
+++ src/App.js
@@ -37,9 +37,15 @@ const App = () => {
 
     service.onTransition(state => {
       console.log(state);
-      // Your code here to change lightStatus when the 
-      //   state of the state machine changes
-
+      switch (state.value) {
+        case 'red': setLightStatus(GREEN_LIGHT);
+          break;
+        case 'green': setLightStatus(YELLOW_LIGHT);
+          break;
+        case 'yellow': setLightStatus(RED_LIGHT);
+          break;
+        default:
+      }
     });
   }, [])
 
